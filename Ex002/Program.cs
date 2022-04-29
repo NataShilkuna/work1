940ad4be5784335a3ea0afe2002c5e2ed95197a5
Natalia <Deva2014@yandex.ru>
2022-04-29 14:07:28 +0300

    Задание_2

---
+++ Ex002/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая приминает на вход три числа и выдаёт максимальное из этих числел.
+Console.WriteLine("Введите два числа: ");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("max={0}", Math.Max(num1,num2));
